00af5d7843e1ee11185bd3a37c7e5845107d1163
Jadran Grba <jadrangrba@gmail.com>
2021-09-13 13:17:13 +0200

    fixing getBooks test

--- BookCatalog.Tests/Domain.Services/BookServiceTest.cs
+++ BookCatalog.Tests/Domain.Services/BookServiceTest.cs
@@ -1,10 +1,7 @@
-﻿using BookCatalog.Common.BindingModels;
-using BookCatalog.Common.BindingModels.Book;
-using BookCatalog.Common.Entities;
+﻿using BookCatalog.Common.Entities;
 using BookCatalog.Common.Helpers;
 using BookCatalog.Common.Interfaces;
 using BookCatalog.Domain.Services;
-using Microsoft.EntityFrameworkCore;
 using MockQueryable.Moq;
 using Moq;
 using System.Collections.Generic;
@@ -30,32 +27,27 @@ namespace BookCatalog.Tests.Domain.Services
                     new Book
                     {
                         Id = 1, Title = "Title1",
-                        Author = "Author1", Note = "Note1", Year = 1,
-                        Publisher = "Publisher", Collection = "Collection1"
+                        Author = "Author1", Note = "Note1", Year = 1
                     },
                     new Book
                     {
                         Id = 2, Title = "Title2",
-                        Author = "Author2", Note = "Note2", Year = 2,
-                        Publisher = "Publisher2", Collection = "Collection2"
+                        Author = "Author2", Note = "Note2", Year = 2
                     },
                     new Book
                     {
                         Id = 3, Title = "Title3",
-                        Author = "Author3", Note = "Note3", Year = 3,
-                        Publisher = "Publisher3", Collection = "Collection3"
+                        Author = "Author3", Note = "Note3", Year = 3
                     },
                     new Book
                     {
                         Id = 4, Title = "Title4",
-                        Author = "Author4", Note = "Note4", Year = 4,
-                        Publisher = "Publisher4", Collection = "Collection4"
+                        Author = "Author4", Note = "Note4", Year = 4
                     },
                     new Book
                     {
                         Id = 5, Title = "Title5",
-                        Author = "Author5", Note = "Note5", Year = 5,
-                        Publisher = "Publisher5", Collection = "Collection5"
+                        Author = "Author5", Note = "Note5", Year = 5
                     }
                 };
 
@@ -65,17 +57,23 @@ namespace BookCatalog.Tests.Domain.Services
                 .Returns(bookMock.Object);
         }
 
-        [Fact]
-        public async Task GetBooks_ReturnsParameterizedPagedBooks()
+        [Theory]
+        [InlineData("Title1", "Author1", 1, "Note1")]
+        [InlineData("Title2", "Author2", 2, "Note2")]
+        [InlineData("Title3", "Author3", 3, "Note3")]
+        [InlineData("Title4", "Author4", 4, "Note4")]
+        [InlineData("Title5", "Author5", 5, "Note5")]
+        public async Task GetBooks_ReturnsSearchedBooksOnly_WhenSearchingByParameters(string title, string author, uint year, string note)
         {
             // Arrange
             var _bookParams = new BookParameters
             {
-                Author = "Author1",
+                Title = title,
+                Author = author,
+                Note = note,
                 PageNumber = 0,
-                PageSize = 5,
-                MinYear = 1,
-                MaxYear = 2
+                MinYear = year,
+                MaxYear = year
             };
 
             // Act
