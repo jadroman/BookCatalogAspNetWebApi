c67b1f91ada11ff203ecd57e503d8ec55efa4f5d
Jadran Grba <jadrangrba@gmail.com>
2023-12-02 17:19:08 +0100

    adds sqlLite as unit test mock

--- BookCatalog.Tests/Domain.Services/BookServiceTest.cs
+++ BookCatalog.Tests/Domain.Services/BookServiceTest.cs
@@ -10,21 +10,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
+using Microsoft.Data.Sqlite;
+using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace BookCatalog.Tests.Domain.Services
 {
-    public class BookServiceTest
+    public class BookServiceTest : IDisposable
     {
         private readonly Mock<IBookCatalogContext> _dbContext;
         private readonly IBookRepository _bookRepo;
+        private readonly IBookRepository _bookRepo2;
         private readonly IMapper _mapper;
 
+        private readonly DbConnection _connection;
+        private readonly DbContextOptions<BookCatalogContext> _contextOptions;
+
         private readonly IBookService _sut;
+        private readonly IBookService _sut2; 
 
         public BookServiceTest()
         {
+            // Create and open a connection. This creates the SQLite in-memory database, which will persist until the connection is closed
+            // at the end of the test (see Dispose below).
+            _connection = new SqliteConnection("Filename=:memory:");
+            _connection.Open();
+
+            // These options will be used by the context instances in this test suite, including the connection opened above.
+            _contextOptions = new DbContextOptionsBuilder<BookCatalogContext>()
+                .UseSqlite(_connection)
+                .Options;
+
+            // Create the schema and seed some data
+            using var context = new BookCatalogContext(_contextOptions);
+            context.Database.EnsureCreated();
+
             _dbContext = new Mock<IBookCatalogContext>();
             _bookRepo = new BookRepository(_dbContext.Object);
+            _bookRepo2 = new BookRepository(context);
+
 
             var config = new MapperConfiguration(c =>
             {
@@ -34,6 +59,7 @@ namespace BookCatalog.Tests.Domain.Services
             _mapper = config.CreateMapper();
 
             _sut = new BookService(_bookRepo, _mapper);
+            _sut2 = new BookService(_bookRepo2, _mapper);
 
             var books = new List<Book>
                 {
@@ -64,11 +90,18 @@ namespace BookCatalog.Tests.Domain.Services
                     }
                 };
 
-            var bookMock = books.AsQueryable().BuildMockDbSet();
+            context.AddRange(books);
+            context.SaveChanges();
 
-            _dbContext.Setup(repo => repo.Books)
-                .Returns(bookMock.Object);
+            //var bookMock = books.AsQueryable().BuildMockDbSet();
+
+            //_dbContext.Setup(repo => repo.Books)
+            //    .Returns(bookMock.Object);
         }
+
+        BookCatalogContext CreateContext() => new BookCatalogContext(_contextOptions);
+
+        public void Dispose() => _connection.Dispose();
 
         [Theory]
         [InlineData("Title1", "Author1", 1, "Note1")]
@@ -87,7 +120,9 @@ namespace BookCatalog.Tests.Domain.Services
             };
 
             // Act
-            var result = await _sut.GetBooks(_bookParams);
+            //var result = await _sut.GetBooks(_bookParams);
+            var srv = new BookService(new BookRepository(new BookCatalogContext(_contextOptions)), _mapper);
+            var result = await srv.GetBooks(_bookParams);
 
             // Assert
             Assert.True(result.Items.Count() == 1);
