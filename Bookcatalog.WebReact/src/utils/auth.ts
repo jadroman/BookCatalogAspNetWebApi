1ab1d18ba16c6e0d0e4e29ef12d7a0009163aafe
Jadran Grba <jadrangrba@gmail.com>
2024-01-25 10:15:15 +0100

    moves interceptors back to auth

--- Bookcatalog.WebReact/src/utils/auth.ts
+++ Bookcatalog.WebReact/src/utils/auth.ts
@@ -1,6 +1,47 @@
 import axios from 'axios';
 import { getApiUrl, refreshTokenUrl } from 'config/url';
 
+axios.interceptors.request.use((config) => {
+    const token = localStorage.getItem("bookCatalogToken");
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    else {
+        config.headers.Authorization = null;
+    }
+
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+    localStorage.removeItem("refreshingTokenRequested");
+
+    return response;
+}, async error => {
+    if (error.response.status === 401) {
+        if (!localStorage.getItem("refreshingTokenRequested")) {
+
+            localStorage.setItem("refreshingTokenRequested", 'true');
+
+            const tokenRefreshSuccessful = await refreshToken();
+
+            if (!tokenRefreshSuccessful) {
+                localStorage.removeItem("bookCatalogToken");
+                localStorage.removeItem("bookCatalogRefreshToken");
+                localStorage.removeItem("bookCatalogUserName");
+                localStorage.removeItem("refreshingTokenRequested");
+
+                window.location.hash = '/login'
+            }
+        }
+    }
+
+    return error;
+});
+
 export const setAuthTokenHeader = (token: string) => {
     if (token) {
         axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
