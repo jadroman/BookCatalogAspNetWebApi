0f9497ecb9483d75949b1b1eef034fbfaf920745
Jadran Grba <jadrangrba@gmail.com>
2024-01-26 17:36:08 +0100

    returns useEffect part because I need a to know when the select value changes internally.

--- Bookcatalog.WebReact/src/components/Category/CategorySelection.tsx
+++ Bookcatalog.WebReact/src/components/Category/CategorySelection.tsx
@@ -1,16 +1,20 @@
 import MenuItem from "@mui/material/MenuItem";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
-import React from "react";
+import React, { useEffect } from "react";
 import { CategorySelectionProps } from "types/props";
 
 
 export default function CategorySelection(props: CategorySelectionProps): JSX.Element {
     const [selectedCategory, setSelectedCategory] = React.useState<string>(props.selectedCategory || '0');
 
+    useEffect(() => {
+        props.onSelectCategory(selectedCategory);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [selectedCategory]);
+
     const selectHandleChange = (event: SelectChangeEvent) => {
         const selectedValue = event.target.value as string
         setSelectedCategory(selectedValue);
-        props.onSelectCategory(selectedValue);
     };
 
     const menuItems = props.inputData.map(i => <MenuItem id={i.id.toString()} key={i.id} value={i.id}>{i.name}</MenuItem>)
